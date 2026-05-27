38486df66b3b6fa3c8ea4927476f0aab320d4432
Gregor Lohaus <gregor.lohaus@8mylez.com>
2026-05-27 15:54:19 +0200

    gradle deps via nix

--- flake.nix
+++ flake.nix
@@ -14,6 +14,48 @@
       jlib = jlibghostty.packages.${system}.jlibghostty;
       graalvm = pkgs.graalvmPackages.graalvm-ce;
       gradle = if pkgs ? gradle_9 then pkgs.gradle_9 else pkgs.gradle;
+      gradleDeps = pkgs.stdenvNoCC.mkDerivation {
+        pname = "jprototerm-gradle-deps";
+        version = "0.1.0";
+        src = ./.;
+
+        nativeBuildInputs = [
+          graalvm
+          gradle
+        ];
+
+        outputHashAlgo = "sha256";
+        outputHashMode = "recursive";
+        outputHash = pkgs.lib.fakeHash;
+
+        buildPhase = ''
+          runHook preBuild
+
+          export HOME=$TMPDIR/home
+          export GRADLE_USER_HOME=$out
+          mkdir -p "$HOME" "$GRADLE_USER_HOME"
+
+          gradle \
+            --no-daemon \
+            --stacktrace \
+            -PjlibghosttyMavenRepo=${jlib}/maven \
+            help
+
+          gradle \
+            --no-daemon \
+            --stacktrace \
+            -PjlibghosttyMavenRepo=${jlib}/maven \
+            dependencies \
+            --configuration runtimeClasspath
+
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          runHook postInstall
+        '';
+      };
     in {
       packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
         pname = "jprototerm";
@@ -32,9 +74,12 @@
           export HOME=$TMPDIR/home
           export GRADLE_USER_HOME=$TMPDIR/gradle
           mkdir -p "$HOME" "$GRADLE_USER_HOME"
+          cp -R ${gradleDeps}/. "$GRADLE_USER_HOME"
+          chmod -R u+w "$GRADLE_USER_HOME"
 
           gradle \
             --no-daemon \
+            --offline \
             --stacktrace \
             -PjlibghosttyMavenRepo=${jlib}/maven \
             nativeCompile
